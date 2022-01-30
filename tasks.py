7361c9659b43033ad19c76c0c1d1e59c2938ed4f
poxiQ <78737354+poxiQ@users.noreply.github.com>
2022-01-30 16:13:36 +0300

    decorator#1

---
+++ tasks.py
@@ -0,0 +1,18 @@
+import time
+
+
+def times(function):
+    def time_time(*args):
+        start = time.time()
+        function(*args)
+        return str(time.time() - start) + " seconds"
+
+    return time_time
+
+
+@times
+def func(a, b):
+    return a + b
+
+
+print(func(10, 5))
